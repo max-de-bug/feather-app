7466b8353d067d9555e82191d457ab7a1a37b40f
Maksym <maksym0.yuzva1@gmail.com>
2023-11-06 19:05:44 +0200

    implementing prisma setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "./lib/utils";
 import Navbar from "./components/Navbar";
-
+import Providers from "./components/Providers";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,15 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="light">
-      <body
-        className={cn(
-          "min-h-screen font-sans antialiased grainy",
-          inter.className
-        )}
-      >
-        <Navbar />
-        {children}
-      </body>
+      <Providers>
+        <body
+          className={cn(
+            "min-h-screen font-sans antialiased grainy",
+            inter.className
+          )}
+        >
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,4 +1,34 @@
-import { router } from "./trpc";
-export const appRouter = router({});
+import { db } from "@/db";
+import { TRPCError } from "@trpc/server";
+import { publicProcedure, router } from "./trpc";
+
+type User = {
+  id: string;
+  email: string;
+};
+
+export const appRouter = router({
+  authCallback: publicProcedure.query<User>(async (user) => {
+    if (!user.id || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!dbUser) {
+      const newUser = await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+      return newUser; // Return the created user
+    }
+
+    return dbUser; // Return the found user
+  }),
+});
 
 export type AppRouter = typeof appRouter;
