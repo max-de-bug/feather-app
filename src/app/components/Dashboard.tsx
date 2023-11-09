553d6ee947cfa1b660b466d707ec0bfbd8ebb681
Maksym <maksym0.yuzva1@gmail.com>
2023-11-09 13:08:13 +0200

    "implementing delete functionality of the file in dashboard component"

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -1,12 +1,20 @@
 "use client";
-import { Ghost } from "lucide-react";
+import { Ghost, MessageSquare, Plus, Trash } from "lucide-react";
 import UploadButton from "./UploadButton";
 
 import { trpc } from "@/app/components/_trpc/client";
 import Skeleton from "react-loading-skeleton";
 import Link from "next/link";
+import { format } from "data-fns";
+import { Button } from "./ui/button";
 const Dashboard = () => {
+  const utils = trpc.useContext();
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+  });
 
   return (
     <main className="mx-auto max-w-7xl md:p-10">
@@ -34,8 +42,33 @@ const Dashboard = () => {
                 >
                   <div className="pt-6 px-6 flex w=full items-center justify-between space-x-6">
                     <div className="h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+                    <div className="flex-1 truncate">
+                      <div className="flex items-center space-x-3  ">
+                        <h3 className="truncate text-lg font-medium text-zinc-900">
+                          {file.name}
+                        </h3>
+                      </div>
+                    </div>
                   </div>
                 </Link>
+                <div className="px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500">
+                  <div className="flex items-center gap-2">
+                    <Plus className="h-4 w-4" />
+                    {format(new Date(file.createdAt), "MMM yyyy")}
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <MessageSquare className="h-4 w-4" />
+                    mocked
+                  </div>
+                  <Button
+                    onClick={() => deleteFile({ id: file.id })}
+                    size="sm"
+                    className="w full"
+                    variant="destructive"
+                  >
+                    <Trash className="h-4 w-4" />
+                  </Button>
+                </div>
               </li>
             ))}
         </ul>
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,50 +3,67 @@ import { TRPCError } from "@trpc/server";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { useContext } from "react";
 import { AuthContext } from "@/app/context/authContex"; // Make sure this path is correct
+import { z } from "zod";
 
 type User = {
   id: string;
   email: string;
 };
 
-const user = useContext(AuthContext); // Make sure you are using useContext within a component
+// const user = useContext(AuthContext); // Make sure you are using useContext within a component
 
 export const appRouter = router({
-  // authCallback: publicProcedure.query<User>(async (user) => {
-  //   if (!user.id || !user.email) {
-  //     throw new TRPCError({ code: "UNAUTHORIZED" });
-  //   }
-  //   const dbUser = await db.user.findFirst({
-  //     where: {
-  //       id: user.id,
-  //     },
-  //   });
-  //   if (!dbUser) {
-  //     const newUser = await db.user.create({
-  //       data: {
-  //         id: user.id,
-  //         email: user.email,
-  //       },
-  //     });
-  //     return newUser; // Return the created user
-  //   }
+  authCallback: publicProcedure.query<User>(async (user) => {
+    if (!user.id || !user.email) {
+      throw new TRPCError({ code: "UNAUTHORIZED" });
+    }
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
+    if (!dbUser) {
+      const newUser = await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+      return newUser; // Return the created user
+    }
 
-  //   return dbUser; // Return the found user
+    return dbUser; // Return the found user
+  }),
+  // registerUser: publicProcedure.mutation<User>(async ({ ctx, input }) => {
+  //   // Here you can register the user with the provided email and password
+  //   const { email, password } = input;
   // }),
-  // registerUser: publicProcedure.mutation<User>(
-  //   async ({ ctx, input }) => {
-  //     // Here you can register the user with the provided email and password
-  //     const { email, password } = input;
-
-  // 	}),
   getUserFiles: privateProcedure.query(async ({ ctx }) => {
     const { userId } = ctx;
 
     return await db.file.findMany({
       where: {
-        userId,
+        userId: "1",
       },
     });
   }),
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async (ctx) => {
+      const { userId } = ctx;
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+      return file;
+    }),
 });
 export type AppRouter = typeof appRouter;
